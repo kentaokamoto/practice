e0097098b16cc85821e6b30d8e308fe6f9a076db
kentaokamoto <kenntabsk@gmail.com>
2021-02-14 18:07:50 +0900

    This is DANGEROUS File DO NOT

--- Lchika.py
+++ Lchika.py
@@ -6,26 +6,22 @@ import time
  
 #RPi.GPIOモジュールをインポート 
 import RPi.GPIO as GPIO
- 
+from pynput.keyboard import Key, Controller
+
 # BCM(GPIO番号)で指定する設定 
 GPIO.setmode(GPIO.BCM)
  
 # GPIO17を出力モード設定 
 GPIO.setup(17, GPIO.OUT)
- 
+
+keyboard = Controller()
 try:
   while True:
-    # GPIO17の出力を1にして、LED点灯 
-    GPIO.output(17, 1)
- 
-    # 0.5秒待つ 
-    time.sleep(0.5)
- 
-    # GPIO17の出力を0にして、LED消灯 
-    GPIO.output(17, 0)
- 
-    # 0.5秒待つ 
-    time.sleep(0.5)
+      if keyboard.press(Key.space):
+          GPIO.output(17, 1)
+          time.sleep(0.5)
+          GPIO.output(17, 0)
+          time.sleep(0.5)
 except KeyboardInterrupt:
   # GPIO設定クリア 
   GPIO.cleanup()
